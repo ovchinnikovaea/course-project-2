d5ceb7d9a98ba842074e1fadbfb534789d3ac2b8
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-01-13 14:40:33 +0300

    added main file

---
+++ src/main/java/pro/sky/java/course2/courseproject2/CourseProject2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.courseproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course2/courseproject2/CourseProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.courseproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
