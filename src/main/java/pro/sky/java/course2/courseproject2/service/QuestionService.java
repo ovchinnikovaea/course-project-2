913e5e39e80bf04f2b7c759ec502a5656bf641b8
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-01-14 18:45:05 +0300

    course project is done

---
+++ src/main/java/pro/sky/java/course2/courseproject2/service/QuestionService.java
@@ -0,0 +1,15 @@
+package pro.sky.java.course2.courseproject2.service;
+
+import pro.sky.java.course2.courseproject2.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+
+    Question add(String question, String answer);
+    Question add(Question question);
+    Question remove(Question question);
+    Collection<Question> getAll();
+    Question getRandomQuestion();
+
+}
